e82468afafc17ec20c4e55570b1df0f166516ea1
USUARIO <rolando>
2017-02-03 13:16:59 -0500

    Made menu links, stilising us page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :pages, only:[:index]
-
   root to: 'pages#index', page: "home"
 
-  get "pages/:page", to: "pages#index", defaults: { page: "home" }
+  get "pages/:page", to: "pages#index", defaults: { page: "home" }, as: :page
 end
